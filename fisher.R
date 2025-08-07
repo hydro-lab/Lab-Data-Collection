18f8e79881e79f8c33982200c2658bc58b5fe383
David Kahler <david.m.kahler@gmail.com>
2025-08-06 21:40:01 -0400

    add download record to track downloads.

--- fisher.R
+++ fisher.R
@@ -8,30 +8,34 @@ library(latex2exp)
 
 # READ IN AND SORT
 
-x <- read_csv("/Volumes/D/Fisher711_Table1.dat", skip = 4, col_names = FALSE)
+x <- read_csv("Fisher711_FisherMet.dat", skip = 4, col_names = FALSE)
+t <- read_csv("FisherHall_download.csv") # all in UTC
+previousDownload <- t$RECORD[nrow(t)]
+download <- data.frame(Sys.Date(), x$X1[nrow(y)], x$X2[nrow(x)])
+write_csv(download, "FisherHall_download.csv", append = TRUE)
 
 y <- x %>%
      rename(TIMESTAMP = X1, 
             RECORD = X2, 
             PTemp_C_Avg = X3, 
-            Rain_mm_Tot = X4, 
-            AirTC_Avg = X5, 
-            AirTC_Std = X6, 
-            RH = X7, 
+            AirTC_Avg = X4, 
+            AirTC_Std = X5, 
+            RH = X6, 
+            BV_BP_Avg = X7, 
             SlrW_Avg = X8, 
-            SlrW_Std = X9, 
-            CS320_Temp_Avg = X10, 
-            CS320_Angle_Avg = X11) %>%
+            CS320_Angle_Avg = X9, 
+            Rain_mm_Tot = X10) %>%
+     filter(RECORD > previousDownload) %>% # this should filter out previous download, record starts at 0, 
      mutate(time_utc = ymd_hms(TIMESTAMP), 
             unix_utc = as.numeric(time_utc), 
             time_et = with_tz(time_utc, tz = "US/Eastern"), 
             utc_offset = (as.numeric(force_tz(time_et, tz = "UTC")) - unix_utc)/3600 ) %>%
-     select(-TIMESTAMP, -RECORD, -SlrW_Std, -CS320_Temp_Avg, -CS320_Angle_Avg, -PTemp_C_Avg)
+     select(-TIMESTAMP, -RECORD, -CS320_Angle_Avg, -PTemp_C_Avg)
 
 # PIVOT TO LONGER FORMAT, ADD SITE, SOURCE, and QC flag
 # LEVEL 0 is raw data
 
-z <- pivot_longer(y, cols = c(AirTC_Avg,AirTC_Std,RH,SlrW_Avg,Rain_mm_Tot),
+z <- pivot_longer(y, cols = c(AirTC_Avg,AirTC_Std,RH,BV_BP_Avg,SlrW_Avg,Rain_mm_Tot),
                   names_to = "Variable",
                   values_to = "Value")
 z$Site <- "Duquesne_FisherHall"
@@ -44,11 +48,11 @@ for (i in 1:n) {
      if (z$Variable[i]=="AirTC_Avg") {z$Method[i] = "Thermometer_hygrometer"}
      else if (z$Variable[i]=="AirTC_Std") {z$Method[i] = "Thermometer_hygrometer"}
      else if (z$Variable[i]=="RH") {z$Method[i] = "Thermometer_hygrometer"}
-     else if (z$Variable[i]=="SlrW_Avg") {z$Method[i] = "ThermopilePyranometer"}
-     else if (z$Variable[i]=="SlrW_Std") {z$Method[i] = "ThermopilePyranometer"}
-     else if (z$Variable[i]=="CS320_Temp_Avg") {z$Method[i] = "ThermopilePyranometer"}
-     else if (z$Variable[i]=="CS320_Angle_Avg") {z$Method[i] = "ThermopilePyranometer"}
-     else if (z$Variable[i]=="Rain_mm_Tot") {z$Method[i] = "HeatedTippingBucket"}
+     else if (z$Variable[i]=="SlrW_Avg") {z$Method[i] = "Thermopile_pyranometer"}
+     else if (z$Variable[i]=="SlrW_Std") {z$Method[i] = "Thermopile_pyranometer"}
+     else if (z$Variable[i]=="CS320_Temp_Avg") {z$Method[i] = "Thermopile_pyranometer"}
+     else if (z$Variable[i]=="CS320_Angle_Avg") {z$Method[i] = "Thermopile_pyranometer"}
+     else if (z$Variable[i]=="Rain_mm_Tot") {z$Method[i] = "Heated_tipping_bucket"}
 }
 
 level0 <- z
@@ -64,93 +68,40 @@ for (i in 1:n) {
           y$AirTC_Std[i] <- -9999
      } else if (y$AirTC_Avg[i] < -40) {
           y$AirTC_Avg[i] <- -9999
-          print(paste0("temperature below valid range ", y$time_utc[i]))
+          print(paste0("Temperature below valid range.  Check UTC: ", y$time_utc[i]))
      } else if (y$AirTC_Avg[i] > 70) {
           y$AirTC_Avg[i] <- -9999
-          print(paste0("temperature above valid range ", y$time_utc[i]))
+          print(paste0("Temperature above valid range.  Check UTC: ", y$time_utc[i]))
      } else if ((i > 4) & (i < (n-4))) {
-          c <- 0 # count
-          t <- 0 # temperature
           s <- 0 # standard deviation
           for (j in (j-4):(j+4)) {
-               if 
+               s <- max(c(max(y$AirTC_Std[(i-4):(i-1)], na.rm = TRUE), max(y$AirTC_Std[(i+1):(i+4)], na.rm = TRUE)))
+               if ( ( y$AirTC_Avg[i] > t+(8*s) ) | ( y$AirTC_Avg[i] < t-(8*s) ) ) {
+                    print(paste0("Spike detected; manual intervention needed at UTC: ", z$time_utc[i]))
+               }
           }
-          s <- max(c(max(y$AirTC_Std[(i-4):(i-1)], na.rm = TRUE), max(y$AirTC_Std[(i+1):(i+4)], na.rm = TRUE)))
-          t <- mean(c(mean(y$AirTC_Avg[(i-4):(i-1)], na.rm = TRUE), mean(y$AirTC_Avg[(i+1):(i+4)], na.rm = TRUE)))
-          if ( ( y$AirTC_Avg[i] > t+(8*s) ) | ( y$AirTC_Avg[i] < t-(8*s) ) ) {
-               print(paste0("spike detected, manual intervention needed ", z$time_utc[i]))
-          }
-     } 
-     if (is.na(x$RHpct_Min[i])) { # NOTE: should also fix to include screening for -9999 or other common error flags
-     x$RHpct_Min[i] <- -9999
-     x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "m,") #
-} else {
-     if (x$RHpct_Min[i] < 0) {
-          if (x$RHpct_Min[i] < -1) {x$RHpct_Min[i] <- -9999}
-          x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "l,")
-     }
-     if (x$RHpct_Min[i] > 100) {
-          if (x$RHpct_Min[i] > 101) {x$RHpct_Min[i] <- -9999}
-          x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "h,")
-     }
-}
-     
-}
-
-if (is.na(x$RHpct_Min[i])) { # NOTE: should also fix to include screening for -9999 or other common error flags
-     x$RHpct_Min[i] <- -9999
-     x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "m,") #
-} else {
-     if (x$RHpct_Min[i] < 0) {
-          if (x$RHpct_Min[i] < -1) {x$RHpct_Min[i] <- -9999}
-          x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "l,")
-     }
-     if (x$RHpct_Min[i] > 100) {
-          if (x$RHpct_Min[i] > 101) {x$RHpct_Min[i] <- -9999}
-          x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "h,")
-     }
-}
-if (is.na(x$RHpct_Max[i])) {
-     x$RHpct_Max[i] <- -9999
-     x$RHpct_Max_qc[i] <- paste0(x$RHpct_Max_qc[i], "m,") #
-} else {
-     if (x$RHpct_Max[i] < 0) {
-          if (x$RHpct_Max[i] < -1) {x$RHpct_Max[i] <- -9999}
-          x$RHpct_Max_qc[i] <- paste0(x$RHpct_Max_qc[i], "l,")
-     }
-     if (x$RHpct_Max[i] > 100) {
-          if (x$RHpct_Max[i] > 101) {x$RHpct_Max[i] <- -9999}
-          x$RHpct_Max_qc[i] <- paste0(x$RHpct_Max_qc[i], "h,")
      }
 }
 
-
-
-
-
-z <- pivot_longer(y, cols = c(AirTC_Avg,AirTC_Std,RH,SlrW_Avg,Rain_mm_Tot),
+z <- y %>%
+     select(AirTC_Avg, time_utc, unix_utc, time_et, utc_offset) %>%
+     pivot_longer(cols = AirTC_Avg,
                   names_to = "Variable",
                   values_to = "Value")
 z$Site <- "Duquesne_FisherHall"
 z$Source <- "DuqSciEng"
 z$QC <- "Level1"
-
-z$Method <- NA
-n <- nrow(z)
-for (i in 1:n) {
-     if (z$Variable[i]=="AirTC_Avg") {z$Method[i] = "Thermometer_hygrometer"}
-     else if (z$Variable[i]=="AirTC_Std") {z$Method[i] = "Thermometer_hygrometer"}
-     else if (z$Variable[i]=="RH") {z$Method[i] = "Thermometer_hygrometer"}
-     else if (z$Variable[i]=="SlrW_Avg") {z$Method[i] = "ThermopilePyranometer"}
-     else if (z$Variable[i]=="SlrW_Std") {z$Method[i] = "ThermopilePyranometer"}
-     else if (z$Variable[i]=="CS320_Temp_Avg") {z$Method[i] = "ThermopilePyranometer"}
-     else if (z$Variable[i]=="CS320_Angle_Avg") {z$Method[i] = "ThermopilePyranometer"}
-     else if (z$Variable[i]=="Rain_mm_Tot") {z$Method[i] = "HeatedTippingBucket"}
-}
+z$Method <- "Thermometer_hygrometer"
 
 level1 <- z
 write_csv(level1, "/Users/davidkahler/Documents/R/Lab-Data-Collection/FisherHall.csv", append = TRUE)
 
+thisMorning <- ymd_h(paste0(Sys.Date(), "T08"), tz = "US/Eastern")
+yesterday <- y %>%
+     filter(time_et <= )
+
+ggplot(yesterday) +
+     geom_line(aes(x = time_et, y = AirTC_Avg))
 
 
 
